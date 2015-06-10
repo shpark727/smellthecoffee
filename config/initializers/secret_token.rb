c7192a60bebbd4cb973f2c1cec7362fdf59e13da
shpark727naver <shpark727@naver.com>
2015-06-10 03:27:39 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smellcoffee::Application.config.secret_key_base = 'acc89022b3983004605873add3e7364f2e4f54f0fa06b499489ddbadc215d920c60e54368c9a55a1d313cdb58893dc1d607f67dcacde6ee9fe8a122eb0610b06'
